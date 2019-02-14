3e1db45ab46d2cf48cfd4893d82a38f1d74f0f83
Achref-Achour <achref5585@gmail.com>
2019-02-14 11:36:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navmenu from './Navmenu.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Navmenu/>
+      </div>
+    );
+  }
+}
+
+export default App;
